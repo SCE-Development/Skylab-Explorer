e6449969629c9181198ff43cb98a40477160c973
Khai Nguyen <khainl1110@gmail.com>
2021-06-22 14:49:20 -0700

    Add some dummy test

--- test/TestComponent.test.tsx
+++ test/TestComponent.test.tsx
@@ -4,10 +4,24 @@
 
 import { render, screen } from '@testing-library/react';
 import TestComponent from '../Components/TestComponent';
+import Home from '../pages/dashboard';
+import Landing from '../pages/landing';
 
- describe('App', () => {
-     test('renders App component', () => {
-         render(<TestComponent/>);
-         screen.debug();
-     });
- });
+// since there's not really anything to test, we will just render each component
+describe('App', () => {
+    test('renders App component', () => {
+        render(<TestComponent/>);
+        screen.debug();
+    });
+});
+
+// Test render Landing or Home will not work since we import css
+// describe('Render pages', () => {
+//     test('Render landing', () => {
+//         render(<Landing />);
+//     });
+
+//     test('Render Dashboard/Home', () => {
+//         render(<Home />);
+//     });
+// });
