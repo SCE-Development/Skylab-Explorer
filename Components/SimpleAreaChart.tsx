a1a19212761294e0eca34b86ed2b5383f0505a7b
Khai <khainl1110@gmail.com>
2021-06-23 15:30:34 -0700

    Khai d3 (#14)
    * Commit before deleting
    
    * Add simpleareachart

---
+++ Components/SimpleAreaChart.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+
+export default function SimpleAreaChart() {
+    const [data, setData] = useState([
+      {
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+      },
+      {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+      },
+      {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+      },
+      {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+      },
+      {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+      },
+      {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+      },
+      {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+      }
+    ]);
+    return(
+        <ResponsiveContainer width = {400} height = {400}>
+            <AreaChart data = {data}>
+                <CartesianGrid />
+                <XAxis dataKey = "name" />
+                <YAxis />
+                <Area type = "monotone" dataKey = "uv" />
+                <Tooltip />
+            </AreaChart>
+        </ResponsiveContainer>
+    );
+}
---
+++ pages/graphs.tsx
@@ -0,0 +1,12 @@
+import SampleLineChart from '../Components/SampleLineChart';
+import SimpleAreaChart from '../Components/SimpleAreaChart';
+
+export default function Graphs() {
+    return(
+        <div>
+            <SampleLineChart/>
+            <h3>Hi</h3>
+            <SimpleAreaChart />
+        </div>
+    );
+}
