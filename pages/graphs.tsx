ac2d03ba382fd657a92a23b469f5b0cab9cfae10
Khai Nguyen <khainl1110@gmail.com>
2021-06-25 15:46:23 -0700

    Working on index/dashboard file

--- pages/graphs.tsx
+++ pages/graphs.tsx
@@ -1,12 +1,7 @@
 import SampleLineChart from '../Components/SampleLineChart';
 import SamplePieChart from '../Components/SamplePieChart';
 import SimpleAreaChart from '../Components/SimpleAreaChart';
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-
-const useStyles = makeStyles((theme) => {
-
-});
 
 export default function Graphs() {
     return(
