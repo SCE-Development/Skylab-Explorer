db99e20c7aa77c97241c1c775cf6f09d451e0cfa
Khai Nguyen <khainl1110@gmail.com>
2021-06-28 07:20:35 -0700

    Using materialUI to theme website, removing css in the process

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import { makeStyles } from '@material-ui/styles';
 
+// https://stackoverflow.com/questions/59145165/change-root-background-color-with-material-ui-theme
 const useStyles = makeStyles({
   '@global': {
     body: {
