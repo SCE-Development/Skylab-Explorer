bb44342266c05861b7dfc57c95c6b8a55f03b9ac
Khai Nguyen <khainl1110@gmail.com>
2021-06-30 12:45:54 -0700

    Working on CustomKeyMetric but sometimes materialUI not working

---
+++ Components/CustomKeyMetric.tsx
@@ -0,0 +1,43 @@
+import { Container, Grid, makeStyles } from "@material-ui/core";
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+
+const useStyles = makeStyles({
+    roundContainer: {
+        borderRadius: '20px',
+        backgroundColor: 'grey',
+        width: 'fit-content',
+        padding: '10px',
+        maxWidth: '15em'
+    },
+    green: {
+        color: 'green',
+        fontSize: '5em'
+    },
+    red: {
+        color: 'red'
+    }
+});
+
+export default function CustomKeyMetric({ isUp }) {
+    const style = useStyles();
+    return(
+        <Container className = {style.roundContainer}>
+            <h3>Commands made</h3>
+            <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+            >
+                <h1>12</h1>
+                {
+                    isUp
+                    ? <ArrowDropUpIcon className = {style.green} />
+                    : <ArrowDropDownIcon className = {style.green} />
+                }
+            </Grid>
+            <h4>+30.0% (3) Better compared to last week</h4>
+        </Container>
+    );
+}
