757e035b8d2e7559a49375fc17f206f5fb252645
Julian Columbres <julian.columbres@gmail.com>
2021-07-21 04:25:49 -0700

    added app light/dark toggle, fixed theme, added padding, restyled discord/corev4 and landing page

--- Components/CustomCardMetric.tsx
+++ Components/CustomCardMetric.tsx
@@ -1,8 +1,12 @@
+<<<<<<< HEAD
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { makeStyles } from '@material-ui/core/styles';
+=======
+import { Typography, Card, CardContent, Grid } from '@material-ui/core';
+>>>>>>> 21da2f0 (added app light/dark toggle, fixed theme, added padding, restyled discord/corev4 and landing page)
 import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
