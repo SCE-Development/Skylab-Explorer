efc4151bab2bb5a46d27b06a48719701f7cd9bd0
Julian Columbres <julian.columbres@gmail.com>
2021-07-30 02:49:26 -0700

    rebase and formatting

--- pages/analytics/testingDiscord.tsx
+++ pages/analytics/testingDiscord.tsx
@@ -27,53 +27,53 @@ const PurpleTypography = withStyles({
 })(Typography);
 
 export default function DiscordPage({ fetchedCommandData }) {
-const [commandData, setCommandData] = useState(fetchedCommandData);
+  const [commandData, setCommandData] = useState(fetchedCommandData);
 
-console.log(commandData);
+  console.log(commandData);
 
-const darkMode = useDarkMode();
-return (
-      <div>
-        <div style={{ minHeight: "150px", width: '100%' }}>
-          <Box display="flex">
-            <Box flexGrow={1}>
-              <Typography variant="h1" display="inline">SCE Analytics</Typography>
-              <PurpleTypography variant="h2" display="inline">&nbsp;Discord</PurpleTypography>
+  const darkMode = useDarkMode();
+  return (
+        <div>
+          <div style={{ minHeight: "150px", width: '100%' }}>
+            <Box display="flex">
+              <Box flexGrow={1}>
+                <Typography variant="h1" display="inline">SCE Analytics</Typography>
+                <PurpleTypography variant="h2" display="inline">&nbsp;Discord</PurpleTypography>
+              </Box>
+              <Box pt={7}>
+              <Button onClick={darkMode.toggle}>Scheme</Button>
+              </Box>
+              <Box pt={7} pl={3}>
+                <Button>Logout</Button>
+              </Box>
             </Box>
-            <Box pt={7}>
-            <Button onClick={darkMode.toggle}>Scheme</Button>
-            </Box>
-            <Box pt={7} pl={3}>
-              <Button>Logout</Button>
-            </Box>
-          </Box>
+            < br/>
+            < Divider style={{ background: "#A5B7F6" }}/>
+          </div>
+          <DropdownFrequency />
           < br/>
-          < Divider style={{ background: "#A5B7F6" }}/>
+          < br/>
+          < br/>
+          <Typography variant="h4">Key Metrics</Typography>
+          < br/>
+          < br/>
+          < br/>
+          <Grid container direction="row" justifyContent="flex-start" alignItems="center" spacing={10}>
+            <Grid item>
+              <CustomKeyMetric title="Commands Made" number={34} isUp={true} />
+            </Grid>
+            <Grid item>
+              <CustomKeyMetric title="Successful Commands" number={45} isUp={false} />
+            </Grid>
+            <Grid item>
+              <CustomKeyMetric title="Failed Commands" isUp={false} />
+            </Grid>
+          </Grid>
+          < br/>
+          < br/>
+          < br/>
+          <Typography variant="h4">Graphs</Typography>
         </div>
-        <DropdownFrequency />
-        < br/>
-        < br/>
-        < br/>
-        <Typography variant="h4">Key Metrics</Typography>
-        < br/>
-        < br/>
-        < br/>
-        <Grid container direction="row" justifyContent="flex-start" alignItems="center" spacing={10}>
-          <Grid item>
-            <CustomKeyMetric title="Commands Made" number={34} isUp={true} />
-          </Grid>
-          <Grid item>
-            <CustomKeyMetric title="Successful Commands" number={45} isUp={false} />
-          </Grid>
-          <Grid item>
-            <CustomKeyMetric title="Failed Commands" isUp={false} />
-          </Grid>
-        </Grid>
-        < br/>
-        < br/>
-        < br/>
-        <Typography variant="h4">Graphs</Typography>
-      </div>
   );
 }
 
--- pages/api/pageVisitsQuery.ts
+++ pages/api/pageVisitsQuery.ts
@@ -7,13 +7,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const data = await pageVisitsQuery();
     return res.status(200).send({ success: true, data });
   } catch (error) {
-    if (error.response) {
-      console.log("API call failed: client response error: Code", error.response.status);
-    } else if (error.request) {
-      console.log("API call failed: client request error");
-    } else {
-      console.log("API call failed: unknown error");
-    }
     const data = {};
       return data;
   }
